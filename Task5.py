625d3f6c7a07692a99aa6889df443d379d8bfe43
Atabek <atabekarapov14@gmail.com>
2020-11-02 12:39:23 +0600

    Week2_Task5

---
+++ Task5.py
@@ -0,0 +1,91 @@
+# TASK5 HOMEWORK -----> ATABEK
+# 1)
+# a = {1, 6, 4, 0, 34, 17, 100, -45}
+# b = {22, 0, 3, 100, 4}
+# c = a & b
+# d = a - b
+# g = a | b
+# print(c)
+# print(d)
+# print(g)
+
+# 2)
+# Aybek = {'Daho', 'ImperiaPizza', 'Freshbox'}
+# Aidai = {'OchakKebab', 'Freshbox'}
+# Azat = {'Freshbox', 'KFC'}
+# Gulzana = {'Daho', 'ImperiaPizza', 'Freshbox', 'KFC', 'OchakKebab'}
+# a = Aybek & Aidai & Azat & Gulzana
+# print(a)
+
+# 3)
+# Djohn = {5, 7, 11, 10, 28}
+# Raychel = {29, 2, 6, 12, 3}
+# Catren = {1, 5, 14, 8, 22}
+# Sam = {19, 20, 3, 11, 10}
+# if Djohn & Raychel:
+#     print(Raychel)
+
+# if Djohn & Catren:
+#     print(Catren)
+
+# if Djohn & Sam:
+#     print(Sam)
+
+# 4)
+# 
+# 1)
+# Ingredients = {'Cheese', 'mushrooms', 'sous', 'shpinat'}
+# Ingredients.add('pomidor')
+# Ingredients.discard('Kolbasa')
+# Ingredients.remove('shpinat')
+# if 'Cheese' in Ingredients:
+#     Ingredients.remove('Cheese')
+#     Ingredients.add('Cheese Motserella')
+
+# print(Ingredients)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
